e3be3a0dc2824108ea434c8a3a1486e7a1a20c34
David Velásquez <zero.blend@gmail.com>
2021-08-06 19:09:41 -0500

    progress with the initial logic

--- keystrokevi.py
+++ keystrokevi.py
@@ -1,37 +1,84 @@
-import tkinter as tk
-from pynput.keyboard import Listener
+from tkinter import *
+from tkinter import ttk
+from pynput.keyboard import Listener, Key
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master=master
-        self.pack()
-        self.create_widgets()
+class App:
+    def __init__(self, root):
+        self.keys_history = []
 
-    def create_widgets(self):
-        self.hi_there=tk.Button(self)
-        self.hi_there["text"]='Hello world\nClick me'
-        self.hi_there['command']=self.say_hi
-        self.hi_there.pack(side='top')
+        root.title('Keycast')
+        root.attributes("-topmost", 1)
+        #root.attributes('-type', 'dock')
+        root.geometry('150x100+100+100')
+        root.resizable(FALSE, FALSE)
 
-        self.quit=tk.Button(self, text='quit', fg='red', command=self.master.destroy)
-        self.quit.pack(side='bottom')
+        mainframe = ttk.Frame(root)
+        mainframe.grid(column=0, row=0)
 
-    def say_hi(self):
-        print('hi tk!')
+        self.key_value = StringVar()
+        key_label = ttk.Label(mainframe, textvariable=self.key_value)
 
-def on_press(key):
-    print(key)
+        self.shift_label = ttk.Label(mainframe, text='Sh', foreground='#CCCCCC')
+        self.control_label = ttk.Label(mainframe, text='Ct')
+        self.alt_label = ttk.Label(mainframe, text='Al')
+        self.super_label = ttk.Label(mainframe, text='Su')
 
-def main():
-    print('hello David')
-    with Listener(
-            on_press=on_press
-            ) as listener:
-        listener.join()
+        key_label.grid(column=0, row=0)
+        self.shift_label.grid(column=1, row=1)
+        self.control_label.grid(column=2, row=1)
+        self.alt_label.grid(column=3, row=1)
+        self.super_label.grid(column=4, row=1)
+
+        self.start_listening()
+
+        self.space = '__'
+        self.shift_char = '||'
+
+    def on_release(self, key):
+        try:
+            if (key == Key.shift):
+                self.keys_history = []
+                self.shift_label['foreground'] = '#CCCCCC'
+                self.key_value.set(''.join(self.keys_history))
+        except:
+            pass
+
+    def on_press(self, key):
+        print(key)
+        # Key.space
+        # Key.enter
+        # Key.shift
+        # Key.ctrl
+        # Key.alt_r
+        # Key.esc
+        # Key.backspace
+        # Key.tab
+        try:
+            if (key == Key.shift):
+                self.keys_history = []
+                self.shift_label['foreground'] = '#000000'
+                self.keys_history.append(self.shift_char)
+
+            if (key == Key.space):
+                self.keys_history.append(self.space)
+            else:
+                self.keys_history.append(key.char)
+        except:
+            pass
+
+        if (len(self.keys_history) == 5):
+            self.keys_history.pop(0)
+
+        self.key_value.set(''.join(self.keys_history))
+
+    def start_listening(self):
+        listener = Listener(
+            on_press=self.on_press,
+            on_release=self.on_release
+            )
+        listener.start()
 
 
-# main()
-root=tk.Tk()
-app=Application(master=root)
-app.mainloop()
+root = Tk()
+App(root)
+root.mainloop()
